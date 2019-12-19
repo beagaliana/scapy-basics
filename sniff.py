62d1700ccc884aaeb894abeffbf873ef68fbd79d
Beatriz Galiana Carballido <beagaliana@riseup.net>
2019-12-19 22:47:15 +0100

    Small modifications on output shown
    Just a small change in order to show both HTTP packets' URL and summary

--- sniff.py
+++ sniff.py
@@ -12,19 +12,21 @@ def sniff_http_packet(packet):
     if(packet.haslayer(HTTPRequest)):
         url = packet[HTTPRequest].Host.decode() + packet[HTTPRequest].Path.decode()
         
-        print("HTTP Packet: %s" % (url))
+        print("HTTP Packet's URL: %s" % (url))
+        print("------------------------------------------")
+        print("HTTP Packet summary:", packet.summary())
 
 def sniff_dns_packet(packet):
     if(packet.haslayer(DNS)):
-        print("DNS Packet:",packet.summary())
+        print("DNS Packet:", packet.summary())
 
 
 
 def main():
-     print("Sniffing IP Packets...")
-     sniff(filter="ip", iface="wlp1s0", prn=sniff_ip_packet)
-#    print("Sniffing HTTP packets...")
-#    sniff(filter="port 80", iface="wlp1s0", prn=sniff_http_packet)
+#     print("Sniffing IP Packets...")
+#     sniff(filter="ip", iface="wlp1s0", prn=sniff_ip_packet)
+     print("Sniffing HTTP packets...")
+     sniff(filter="port 80", iface="wlp1s0", prn=sniff_http_packet)
 #    print("Sniffing DNS packets")
 #    sniff(filter="port 53", iface="wlp1s0", prn=sniff_dns_packet)
         
