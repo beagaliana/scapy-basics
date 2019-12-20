7e1047638abbe1a053b12e8724b8457234864e9e
Beatriz Galiana Carballido <beagaliana@riseup.net>
2019-12-20 08:23:33 +0100

    Delete unnecessary function

--- read_pcap.py
+++ read_pcap.py
@@ -1,14 +1,5 @@
 from scapy.all import *
 
-#capturaPackets = rdpcap('pcap.pcapng')
-def show_packets(capturaPackets):
-    for index, packet in enumerate(capturaPackets):
-        print("SHOW PACKET " + str(index) + "\n ")
-        packet.show()
-        print("########## END OF THE PACKET " + str(index) + " ##########" + "\n")
-    print("TOTAL NUMBER OF PACKETS: " + str(index + 1))
-
-
 def packets_summary(packets):
     for index, packet in enumerate(packets):
         print("PACKET SUMMARY " + str(index) + " ->  " + packet.summary() + "\n")
